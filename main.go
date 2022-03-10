14d589e502f7bd17e776b734ae323d085778ce92
Dwi Haryani <dwiharyani304@gmail.com>
2022-03-10 17:06:30 +0800

    Init project

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+import (
+	"godwi/handler"
+	"github.com/gin-gonic/gin"
+
+	"godwi/user"
+	"godwi/auth"
+	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	
+  )
+  
+
+func main(){
+	dsn := "root:@tcp(127.0.0.1:3306)/dwigolang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	authService := auth.NewService()
+
+	userHandler := handler.NewUserHandler(userService,authService)
+	
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+	router.Run()
+
+
+	}
+	
+
+
+
